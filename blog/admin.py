1d32362eccd7de4005d00ee49b165a5a63ca3ccb
Ahmed Abdulla <ahmed.abdulla.abdulla@gmail.com>
2023-04-05 15:13:37 +0000

    Finish Generic relationships

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from blog.models import Tag, Post
+from blog.models import Tag, Post, Comment
 admin.site.register(Tag)
 
 class PostAdmin(admin.ModelAdmin):
@@ -10,3 +10,4 @@ class PostAdmin(admin.ModelAdmin):
   list_display = ['title', 'created_at', 'published_at', 'slug']
 
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment)
